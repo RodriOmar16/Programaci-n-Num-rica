a1a3bb746534ed87aa20e897d119080d917b1613
Rodrigo <rodrigoomarmiranda1@gmail.com>
2024-08-26 18:56:04 -0300

    26082024_R2

--- FuncionesEmpiricos/src/funcionEmpirica.java
+++ FuncionesEmpiricos/src/funcionEmpirica.java
@@ -7,4 +7,38 @@ public class funcionEmpirica {
 		this.listPtos = new double[n][2];
 		this.cant 	  = n;
 	}
+	
+	//GETTER AND SETTERS
+	public double[][] getListaPtos(){
+		return this.listPtos;
+	}
+	public void setListaPtos(double l[][], int n) {
+		setCantidad(n);
+		for(int i=0; i<n ;i++) {
+			for(int j=0; j<2 ;j++) {
+				this.listPtos[i][j] = l[i][j];
+			}
+		}
+	}
+	
+	public int getCantidad() {
+		return this.cant;
+	}
+	public void setCantidad(int n) {
+		this.cant = n;
+	}
+	
+	public double getA() {
+		return this.a;
+	}
+	public void setA(double a1) {
+		this.a = a1;
+	}
+	
+	public double getB() {
+		return this.b;
+	}
+	public void setB(double b1) {
+		this.b = b1;
+	}
 }
