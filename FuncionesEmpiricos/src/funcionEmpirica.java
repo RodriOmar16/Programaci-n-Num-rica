7d6ff4b766bb85b5ddd6a686ae14a3304a18fdf2
Rodrigo <rodrigoomarmiranda1@gmail.com>
2024-08-26 18:48:34 -0300

    26082024_R1

---
+++ FuncionesEmpiricos/src/funcionEmpirica.java
@@ -0,0 +1,10 @@
+public class funcionEmpirica {
+	public double listPtos[][];
+	public int cant;
+	public double a, b;
+	
+	public funcionEmpirica(int n){
+		this.listPtos = new double[n][2];
+		this.cant 	  = n;
+	}
+}
